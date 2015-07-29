67ae28689d88aee14dafdc126a49d72b737643ef
jszum <jaroslaw.szumega@gmail.com>
2015-07-29 13:47:45 +0200

    Opening file, reading samples

--- binconverter.py
+++ binconverter.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python
 
 import sys
+import array
+import numpy as np
+
+class Reader:
+
+    def __init__(self):
+        self.data = ''
+        
+
+    def open(self, filename):
+        f = open(filename, 'rb')
+        self.data = np.fromfile(f, dtype=np.int16)
+
+    def convert(self):
+        print 'c'
+
 
 if __name__=="__main__":
     name = sys.argv[1]
-    print name
+    read = Reader()
+    read.open(name)
